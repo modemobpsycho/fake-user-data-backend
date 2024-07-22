2b8099142c5eb64a2e0a03e7cf27534fe060b64a
Vadim Taratuta <hawaiianwork@mail.ru>
2024-07-23 00:34:20 +0300

    added cors frontend url

--- server.ts
+++ server.ts
@@ -7,14 +7,14 @@ const app = express()
 app.use(express.json())
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "*/*"]
+  origin: ["https://itra-task-5-frontend.vercel.app", "http://localhost:5173"]
 }
 
 app.use(cors(corsOptions))
 
 app.use("/api", personRouter)
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
   console.log(`Server is running on localhost:${PORT}`)
